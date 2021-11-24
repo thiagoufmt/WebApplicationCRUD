48e448ce1f6ce0b9e365162c1ac430e4d521beaa
Thiago Elias <thiago.elias@ufmt.br>
2021-11-24 08:41:05 -0400

    Projeto CRUD Visual Studio

---
+++ WebApplicationCRUD/Data/WebApplicationCRUDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationCRUD.Models;
+
+namespace WebApplicationCRUD.Data
+{
+    public class WebApplicationCRUDContext : DbContext
+    {
+        public WebApplicationCRUDContext (DbContextOptions<WebApplicationCRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationCRUD.Models.Department> Department { get; set; }
+    }
+}
